88b6286ddc75722c99271129f89192a269e2bbdd
Altair Dias <89491054+altairfd@users.noreply.github.com>
2022-08-04 22:10:20 -0300

    Add files via upload

---
+++ Aula-04-08/aula.py
@@ -0,0 +1,29 @@
+# # # nI1 = int(input("Valor númeor inteiro"))
+# # # nI2 = int(input("Valor númeor inteiro"))
+# # # nD = float(input("Valor decimal"))
+# #
+# #
+# # resultado = nI1*3 + nD
+# # print(resultado)
+# #
+# # resultado = (nI1*2) + (nI2/2)
+# # print(resultado)
+#
+# resutlado = nD**3
+# print(resultado)
+
+
+# file = open("text.txt", 'r')
+#
+# for teste in file:
+#     print(teste)
+#
+# file.close()
+
+
+for i in range(10):
+    lista = input("valor")
+
+file = open("text.txt", 'w')
+file.write([str(lista])
+file.close()
